e42d19d303c1cce046a82dcc96b80ae71364754d
Naomi Higareda <nfhigareda@actx.edu>
2025-03-20 12:47:04 -0500

    Add project files.

---
+++ wk9quiz9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wk9quiz9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("What is our last name?");
+            string userLastName = Console.ReadLine();
+
+            Console.WriteLine("What is you age?");
+            string userAge = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string userFavoriteColor = Console.ReadLine();
+
+           Console.WriteLine("Hello,"+ userName+ userLastName + "!" + "You are" +  userAge + "years old and your favorite color is" + userFavoriteColor);
+        }
+    }
+}
